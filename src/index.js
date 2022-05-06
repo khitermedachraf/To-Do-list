c8c39aed1c8248c5a6ddb21b752891ef4946d416
KHITER Med Achraf <khiter.med7@gmail.com>
2022-05-06 02:46:09 +0100

    Add Js to render the dynamically created list of tasks

--- src/index.js
+++ src/index.js
@@ -4,28 +4,34 @@ import './style.css';
 const todoTasks = [
   {
     index: 0,
-    description: 'oneoneoneoneoneone',
+    description: 'zerozerozerozero',
     completed: false,
   },
   {
     index: 1,
-    description: 'twotwotwotwotwotwo',
-    completed: false,
+    description: 'oneoneoneoneoneone',
+    completed: true,
   },
   {
     index: 2,
-    description: 'threethreethreethreethreethree',
+    description: 'twotwotwotwotwotwo',
+    completed: true,
+  },
+  {
+    index: 3,
+    description: 'threethreethreethree',
     completed: false,
   },
 ];
 
 const todoList = document.getElementById('todo-list');
 
+// Write a function to iterate over the tasks array and
+// populate an HTML list item element for each task.
 const displayTasks = (todoTasks) => {
   for (let i = 0; i < todoTasks.length; i++) {
-    let task;
+    const task = document.createElement('li');
     task.innerHTML = `
-    <li>
     <div>
       <input
         type="checkbox"
@@ -34,7 +40,7 @@ const displayTasks = (todoTasks) => {
         value="task${todoTasks[i].index}"
         class="checkbox"
       />
-      <label for="task${todoTasks[i].index}">task${todoTasks[i].description}</label>
+      <label for="task${todoTasks[i].index}">${todoTasks[i].description}</label>
     </div>
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +53,12 @@ const displayTasks = (todoTasks) => {
         d="M9.5 13a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0z"
       />
     </svg>
-    </li>
     `;
+    todoList.appendChild(task);
   }
 };
+
+// On page load render the dynamically created list of tasks in the dedicated placeholder.
+window.addEventListener('load', () => {
+  displayTasks(todoTasks);
+});
