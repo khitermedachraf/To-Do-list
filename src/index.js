0690f76da3da2c60089198fab75cf2afa5933a62
KHITER Med Achraf <khiter.med7@gmail.com>
2022-05-06 02:10:15 +0100

    Create todoTasks array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const todoTasks = [
+  {
+    index: 0,
+    description: 'oneoneoneoneoneone',
+    completed: false,
+  },
+  {
+    index: 1,
+    description: 'twotwotwotwotwotwo',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'threethreethreethreethreethree',
+    completed: false,
+  },
+];
