8f6e01eb186eaedeb274b4569f55b32ec5a5dd19
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-05-12 13:02:37 +0100

    Add the remove function test

--- src/__tests__/add-remove.test.js
+++ src/__tests__/add-remove.test.js
@@ -6,11 +6,16 @@ import Actions from '../modules/actions.js';
 
 document.body.innerHTML = '<span id="todo-box"></span>';
 
+const task2 = jest.fn({
+  id: 2,
+  description: 'task3',
+  completed: false,
+});
 describe('test the Add item function', () => {
   test('should create new items to test localStorage the Dom', () => {
     // Arrange
     const task = jest.fn({
-      id: 3,
+      id: 1,
       description: 'task3',
       completed: false,
     });
@@ -18,8 +23,17 @@ describe('test the Add item function', () => {
     // Act
     Actions.addTask(task);
     // the addTask function returns the new array from localStorage which is empty initially
-    const addTask = Actions.addTask(task);
+    const addTask = Actions.addTask(task2);
     // Assert
     expect(addTask).toHaveLength(2);
+  });
+
+  test('should delete an item from localStorage the Dom', () => {
+    // Act
+    Actions.removeTask(task2);
+    // the addTask function returns the new array from localStorage which is empty initially
+    const removeTask = Actions.removeTask(task2);
+    // Assert
+    expect(removeTask).toHaveLength(1);
   });
 });
